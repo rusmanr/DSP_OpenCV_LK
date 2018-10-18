d9d683bc53b49fa80a30d36e14388715758a78aa
rusman rusyadi <rusmanr@yahoo.com>
2018-10-18 07:41:02 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.setWindowTitle(QString("openCV HoughLine V %1:%2").arg(CV_MAJOR_VERSION).arg(CV_MINOR_VERSION));
+    w.setWindowTitle(QString("openCV LK optical flow and HoughLine V %1:%2").arg(CV_MAJOR_VERSION).arg(CV_MINOR_VERSION));
     w.show();
 
     return a.exec();
